1a473efe6508172bac8ffb465e69be73953d55a6
Philona Reetha Sebastian <60103472+philona123@users.noreply.github.com>
2021-10-01 00:13:57 +0530

    Update smallestinarray.cpp

--- smallestinarray.cpp
+++ smallestinarray.cpp
@@ -18,7 +18,13 @@ int main()
     }
     
     int smallest = arr[0];   // initialize smallest to arr[0]
-    
+    for(int i=1; i<size; i++)  
+    {
+        if(arr[i]<smallest)   // compare current element with smallest
+        {
+            smallest = arr[i];
+        }
+    }
     
     cout << “Smallest Element in the array : “ << endl << smallest;  // display result
 
